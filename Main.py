d4504bf18e1c754cf69e11c95596b8091570d305
nkrobinson <kylenr23@gmail.com>
2017-09-20 14:26:34 +0100

    Updated code to python 3.5. Fixed syntax errors.

--- Main.py
+++ Main.py
@@ -5,7 +5,7 @@
 
 import discord
 import asyncio
-import youtube_dl
+# import youtube_dl
 import sys
 import re
 
@@ -16,18 +16,18 @@ class DiscordBot(discord.Client):
         super().__init__()
         self.queue = asyncio.Queue()
 
-    @asyncio.coroutine
-    def on_ready(self):
+    async def on_ready(self):
         print('Logged in as')
         print(self.user.name)
         print(self.user.id)
         print('------')
 
-    @discord.coroutine
-    def on_message(self, message):
+    async def on_message(self, message):
         if message.content.startswith('!'):
             messageSplit = re.split(" ", message.content)
             if messageSplit[0] == '!test':
+                await self.send_message(message.channel, 'Test reply')
+            elif messageSplit[0] == '!count':
                 counter = 0
                 tmp = await self.send_message(message.channel, 'Calculating messages...')
                 async for log in self.logs_from(message.channel, limit=100):
@@ -41,37 +41,29 @@ class DiscordBot(discord.Client):
                 await asyncio.sleep(5)
                 await self.send_message(message.channel, 'Done Sleeping')
             elif messageSplit[0] == '!play':
-                if messageSplit[1] == 'queue':
-                    # Play queue
-                    await self.send_message(message.channel, 'Added to Queue')
-                    pass
-                elif messageSplit[1] == 'remove':
-                    # Play remove 'queue index'
-                    await self.send_message(message.channel,
-                                            'Removed from Queue')
-                    pass
-                else:
+                try:
+                    if messageSplit[1] == 'queue':
+                        # Play queue
+                        await self.send_message(message.channel, 'Added to Queue')
+                    elif messageSplit[1] == 'remove':
+                        # Play remove 'queue index'
+                        await self.send_message(message.channel, 'Removed from Queue')
+                except:
                     # Play 'url'
                     await self.send_message(message.channel, 'Playing')
-                    pass
-
-                pass
             elif messageSplit[0] == '!event':  # Events
-                if messageSplit[1] == 'cancel':
-                    # Event cancel 'id'
-                    await self.send_message(message.channel, 'Cancelled Event')
-                    pass
-                elif messageSplit[1] == 'set':
-                    # Event set 'Title' 'Time' 'Date'
-                    await self.send_message(message.channel, 'New Event')
-                    pass
-                else:
-                    # Event
+                try:
+                    if messageSplit[1] == 'cancel':
+                        # Event cancel 'id'
+                        await self.send_message(message.channel, 'Cancelled Event')
+                    elif messageSplit[1] == 'set':
+                        # Event set 'Title' 'Time' 'Date'
+                        await self.send_message(message.channel, 'New Event')
+                except:
+                    # Current Event
                     await self.send_message(message.channel, 'Current Event')
-                    pass
-                pass
 
 
-if __name__ = '__main__':
+if __name__ == '__main__':
     bot = DiscordBot()
     bot.run(sys.argv[1])
